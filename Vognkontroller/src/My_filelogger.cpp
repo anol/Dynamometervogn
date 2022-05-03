ad8c7a171a897de43b6822befbd42a6658330ba7
anol <aeolsen@gmail.com>
2022-05-03 12:52:13 +0200

    Change data/time format in the log file

--- Vognkontroller/src/My_filelogger.cpp
+++ Vognkontroller/src/My_filelogger.cpp
@@ -8,17 +8,23 @@
 
 #include "My_filelogger.h"
 
-My_filelogger::~My_filelogger(){
-    if(the_file.is_open()){
+My_filelogger::~My_filelogger() {
+    if (the_file.is_open()) {
         the_file.close();
     }
 }
 
-void My_filelogger::initialize(){
+void My_filelogger::initialize() {
     the_file.open("Vognkontroller.txt", std::fstream::out | std::fstream::app);
+    std::time_t result = std::time(nullptr);
+    the_file << ";" << std::endl;
+    the_file << std::put_time(std::localtime(&result), "%H%M%S:Y=%Y%m%d") << ";"
+             << std::endl;
+
 }
 
-void My_filelogger::logg(char key, double data){
+void My_filelogger::logg(char key, double data) {
     std::time_t result = std::time(nullptr);
-    the_file << std::put_time(std::localtime(&result), "%Y%m%d-%H%M%S" ) << ":" << key << "=" << data << ";" << std::endl;
+    the_file << std::put_time(std::localtime(&result), "%H%M%S") << ":" << key << "=" << data << ";"
+             << std::endl;
 }
