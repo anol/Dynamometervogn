492491d76695061c55af179c6541820090513bce
anol <aeolsen@gmail.com>
2022-07-02 17:38:44 +0200

    Vognkontroller: changed logging, millisecond timestamp, filename is date and time.

--- Vognkontroller/src/My_filelogger.cpp
+++ Vognkontroller/src/My_filelogger.cpp
@@ -2,7 +2,6 @@
 // Created by aeols on 01.05.2022.
 //
 
-#include <ctime>
 #include <iostream>
 #include <iomanip>
 
@@ -15,17 +14,19 @@ My_filelogger::~My_filelogger() {
 }
 
 void My_filelogger::initialize() {
-    the_file.open("Vognkontroller.txt", std::fstream::out | std::fstream::app);
-    std::time_t result = std::time(nullptr);
-    the_file << ";" << std::endl;
-    the_file << ";" << std::endl;
-    the_file << std::put_time(std::localtime(&result), "%H%M%S:Y=%Y%m%d") << ";" << std::endl;
-    the_file << ";" << std::endl;
-    the_file << ";" << std::endl;
+    char filename[64] = {"vognkontroller.log"};
+    the_start_time = std::time(nullptr);
+    if (std::strftime(filename, sizeof(filename), "%Y-%m-%dT%H-%M-%S.log", std::localtime(&the_start_time))) {
+    }
+    the_file.open(filename, std::fstream::out | std::fstream::app);
+    the_file << "time(ms),key,data;" << std::endl;
+    the_zero_time = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now().time_since_epoch());
+    logg('X', 0.0);
 }
 
 void My_filelogger::logg(char key, double data) {
-    std::time_t result = std::time(nullptr);
-    the_file << std::put_time(std::localtime(&result), "%H%M%S") << ":" << key << "=" << data << ";"
-             << std::endl;
+    auto timestamp = std::chrono::duration_cast<std::chrono::milliseconds>(
+            std::chrono::system_clock::now().time_since_epoch()) - the_zero_time;
+    the_file << timestamp.count() << "," << key << "," << data << ";" << std::endl;
 }
--- Vognkontroller/src/My_filelogger.h
+++ Vognkontroller/src/My_filelogger.h
@@ -5,10 +5,14 @@
 #ifndef VOGNKONTROLLER_MY_FILELOGGER_H
 #define VOGNKONTROLLER_MY_FILELOGGER_H
 
+#include <ctime>
+#include <chrono>
 #include <fstream>
 
 class My_filelogger {
     std::fstream the_file{};
+    std::time_t the_start_time{};
+    std::chrono::milliseconds the_zero_time{};
 
 public:
     My_filelogger() = default;
