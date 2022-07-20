89e71c48b9ebacff0d91cf72f8fad65c049c4505
anolsen <anders.olsen@ideas.no>
2022-07-20 23:37:23 +0200

    Vognkontroller: updated Odometer to send tation count, F=forward, B=backward, o=strange state, and send 'O=ODOMETER' at startup and as response to a ';' command.

--- Arduino/Odometer/Odometer.ino
+++ Arduino/Odometer/Odometer.ino
@@ -3,21 +3,30 @@ const byte pin_A = 2;
 const byte pin_B = 3;
 volatile byte state = 0;
 volatile byte toggle_LED = LOW;
-volatile double trip = 0.0;
+volatile int trip = 0;
+String my_command = NULL;
+
+// Arduino Uno
+#define digitalPinToInterrupt(p) ( (p) == 2 ? 0 : ((p) == 3 ? 1 : -1) )
 
 void setup() {
     Serial.begin(115200);
     pinMode(pin_LED, OUTPUT);
     pinMode(pin_A, INPUT_PULLUP);
     pinMode(pin_B, INPUT_PULLUP);
-    Serial.println(";ODOMETER;");
+    Serial.println("O=ODOMETER;");
     delay(1000);
     attachInterrupt(digitalPinToInterrupt(pin_A), blink, CHANGE);
     attachInterrupt(digitalPinToInterrupt(pin_B), blink, CHANGE);
+    Serial.setTimeout(1000);
 }
 
 void loop() {
     delay(1000);
+    my_command = Serial.readStringUntil(';');
+    if(my_command != NULL){
+        Serial.println("O=ODOMETER;");
+    }
     toggle_LED = !toggle_LED;
     digitalWrite(pin_LED, toggle_LED);
 }
@@ -29,22 +38,22 @@ void blink() {
     switch (state) {
         case 130:
         case 180: {
-            trip += 0.314;
-            Serial.print("d=");
+            trip ++;
+            Serial.print("F=");
             Serial.print(trip);
             Serial.println(";");
             break;
         }
         case 120:
         case 184: {
-            trip -= 0.314;
-            Serial.print("d=");
+            trip --;
+            Serial.print("B=");
             Serial.print(trip);
             Serial.println(";");
             break;
         }
         default: {
-            Serial.print("s=");
+            Serial.print("o=");
             Serial.print(state, BIN);
             Serial.println(";");
             break;
