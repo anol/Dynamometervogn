eb7428aa09b6489670e36e5c90adfa3596ac5e6b
anolsen <anders.olsen@ideas.no>
2022-07-27 22:35:13 +0200

    Vognkontroller: updated manometer to send on change only

--- Arduino/Manometer/Manometer.ino
+++ Arduino/Manometer/Manometer.ino
@@ -2,6 +2,9 @@
 const int pin_a = A2;
 const int pin_b = A4;
 
+int old_A = 0;
+int old_B = 0;
+
 void setup() {
     Serial.begin(115200);
     Serial.println(";MANOMETER;");
@@ -9,11 +12,19 @@ void setup() {
 }
 
 void loop() {
-    Serial.print("a=");
-    Serial.print(analogRead(pin_a));
-    Serial.println(";");
-    Serial.print("b=");
-    Serial.print(analogRead(pin_b));
-    Serial.println(";");
+    int val_A = analogRead(pin_a);
+    int val_B = analogRead(pin_b);
+    if (val_A != old_A) {
+        old_A = val_A;
+        Serial.print("a=");
+        Serial.print(val_A);
+        Serial.println(";");
+    }
+    if (val_B != old_B) {
+        old_B = val_B;
+        Serial.print("b=");
+        Serial.print(val_B);
+        Serial.println(";");
+    }
     delay(100);
 }
